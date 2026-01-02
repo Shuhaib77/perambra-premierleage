afdf156cf278635e5500030650976b95d12d8322
Shuhaib77 <shuhaibbasheer20022@gmail.com>
2026-01-02 06:30:59 +0530

    updated

--- src/pages/nonauth/home/layout/contact/Contact.jsx
+++ src/pages/nonauth/home/layout/contact/Contact.jsx
@@ -31,7 +31,7 @@ function Contact() {
         className=" h-full    min-h-[1100.666748046875px] w-[100%] flex flex-col justify-center items-center mt-20 p-3 lg:p-10 pt-20  bg-cover bg-no-repeat"
         style={{
           backgroundImage:
-              'linear-gradient(to right, #1C398E/80, #162456),url("https://images.unsplash.com/photo-1624194611924-bb02300dad6c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxjcmlja2V0JTIwYmF0JTIwYmFsbHxlbnwxfHx8fDE3NjcyNjg1NTJ8MA&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral")',
+              'linear-gradient(to right, #1C398E, #162456),url("https://images.unsplash.com/photo-1624194611924-bb02300dad6c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxjcmlja2V0JTIwYmF0JTIwYmFsbHxlbnwxfHx8fDE3NjcyNjg1NTJ8MA&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral")',
         }}
       >
         
